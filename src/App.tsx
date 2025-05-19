53a0487db3ca14263f3b696611032fa6590f4566
Alantrizotte06 <trizotte.alan@gmail.com>
2025-05-19 11:53:30 -0300

    Componente App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-// import React from "react";
-// import { ThemeProvider } from "styled-components";
-// import { BrowserRouter } from "react-router-dom";
-// import { Router } from "./pages/Router";
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter } from "react-router-dom";
+import { Router } from "./pages/Router";
 
-import { Home } from "./Home";
-
-// import { defaultTheme } from "./styles/themes/default";
-// import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <Home />
-    // <ThemeProvider theme={defaultTheme}>
-    //   <BrowserRouter>
-    //     <Router />
-    //   </BrowserRouter>
-    //   <GlobalStyle />
-    // </ThemeProvider>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+      <GlobalStyle />
+    </ThemeProvider>
   );
 }
